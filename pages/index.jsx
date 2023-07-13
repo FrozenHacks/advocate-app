c0042704afbf5a17e1d9e8373ecfce2376d3f628
blank_zs <thiru.sanjaykumar@gmail.com>
2023-07-13 15:45:34 +0530

    UI

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,31 +2,80 @@ import Layout from '../components/layout'
 import { getCookie } from 'cookies-next';
 import Link from 'next/link'
 
-export default function HomePage( {username} ) {
-    return (
-        <Layout pageTitle="Home">
-        {username ?
-        <>
-            <h2>Hi {username}</h2>
-            <Link href="/profile">Profile</Link><br/>
-            <Link href="/api/logout">Logout</Link>
-        </>: 
-        <>
-            <h2>Log in</h2>
-            <Link href="/login">Login</Link><br/>
+export default function HomePage({ username }) {
+  return (
+    <Layout pageTitle="Home">
+      <header>
+        <nav className="navbar">
+          <div className="logo">
+            <Link href="/">Voting Website</Link>
+          </div>
+          <div className="nav-links">
+            <ul>
+              {username ? (
+                <>
+                  <li>
+                    <Link href="/profile">Profile</Link>
+                  </li>
+                  <li>
+                    <button className="logout-button">
+                      <Link href="/api/logout">Logout</Link>
+                    </button>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li>
+                    <Link href="/login">Login</Link>
+                  </li>
+                  <li>
+                    <Link href="/signup">Signup</Link>
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
+        </nav>
+      </header>
+      {/* <div className="container">
+        <h2>Home</h2>
+        <div className="input-container">
+          <input
+            minLength="3"
+            name="username"
+            id="username"
+            type="text"
+            placeholder="Username"
+            required
+          />
+          <input
+            minLength="5"
+            name="password"
+            id="password"
+            type="password"
+            placeholder="Password"
+            required
+          />
+        </div> */}
+        {/* <div className="button-container">
+          <button className="auth-button">
+            <Link href="/login">Login</Link>
+          </button>
+          <button className="auth-button">
             <Link href="/signup">Signup</Link>
-        </>
-        }
-        </Layout>
-    );
+          </button>
+        </div> */}
+      {/* </div> */}
+    </Layout>
+  );
 }
 
 export async function getServerSideProps(context) {
-    const req = context.req
-    const res = context.res
-    var username = getCookie('username', { req, res });
-    if (username == undefined){
-        username = false;
-    }
-    return { props: {username} };
-};
+  const req = context.req
+  const res = context.res
+  var username = getCookie('username', { req, res });
+  if (username == undefined) {
+    username = false;
+  }
+  return { props: { username } };
+}
--- pages/login.jsx
+++ pages/login.jsx
@@ -3,40 +3,60 @@ import { getCookie } from 'cookies-next';
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 
-export default function LoginPage( {username} ) {
-    const router = useRouter()
-    const { msg } = router.query
-    return (
-        <Layout pageTitle="Login">
-            <Link href="/">Home</Link><br/>
-            {msg ?
-                <h3 className="red">{msg}</h3>
-            :
-                <></>
-            }
-            <h2>Log in</h2>
-            <div>
-            <form action='/api/login' method='POST'>
-                <input minLength="3" name="username" id="username" type="text" placeholder='username' required></input><br/>
-                <input minLength="5" name="password" id="password" type="password" placeholder='password' required></input><br/>
-                <input type="submit" value="Login"/>
-            </form>
-            </div>
-        </Layout>
-    );
+export default function LoginPage({ username }) {
+  const router = useRouter()
+  const { msg } = router.query
+  return (
+    <Layout pageTitle="Login">
+      <div className="container">
+        {/* <Link href="/">Home</Link> */}
+        <p>Welcome to Voting Site</p>
+        {msg ? (
+          <h3 className="red">{msg}</h3>
+        ) : (
+          <></>
+        )}
+        <h2>Log in</h2>
+        <div className="login-form">
+          <form action="/api/login" method="POST">
+            <input
+              minLength="3"
+              name="username"
+              id="username"
+              type="text"
+              placeholder="Username"
+              required
+            />
+            <br />
+            <input
+              minLength="5"
+              name="password"
+              id="password"
+              type="password"
+              placeholder="Password"
+              required
+            />
+            <br />
+            <input type="submit" value="Login" />
+            <button onCl></button>
+          </form>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 export async function getServerSideProps(context) {
-    const req = context.req
-    const res = context.res
-    var username = getCookie('username', { req, res });
-    if (username != undefined){
-        return {
-            redirect: {
-                permanent: false,
-                destination: "/"
-            }
-        }
+  const req = context.req
+  const res = context.res
+  var username = getCookie('username', { req, res });
+  if (username != undefined) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/"
+      }
     }
-    return { props: {username:false} };
-};
+  }
+  return { props: { username: false } };
+}
